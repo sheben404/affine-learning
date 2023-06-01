06cedee838d070b4bdeff92a166348e9659fd663
Kehan Wang <sheben404@gmail.com>
2023-06-01 17:37:18 +0800

    feat: add workspace && switch workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,79 @@
 import '@blocksuite/editor/themes/affine.css'
 import './App.css'
 import { Editor } from './components/Editor'
-import { useAtomValue, useAtom } from 'jotai'
+import { useAtomValue, useAtom, useSetAtom } from 'jotai'
 import { workspaceIdsAtom, currentWorkspaceIdAtom } from './store'
-import { Suspense } from 'react'
+import { ChangeEvent, Suspense, useCallback, useEffect, useState } from 'react'
+
+const CreateWorkspace = () => {
+  const [name, setName] = useState('')
+  const setWorkspaceIds = useSetAtom(workspaceIdsAtom)
+  return (
+    <div>
+      No Workspace
+      <input
+        value={name}
+        onChange={useCallback((e: ChangeEvent<HTMLInputElement>) => {
+          setName(e.target.value)
+        }, [])}
+      />
+      <button
+        onClick={useCallback(() => {
+          setWorkspaceIds((ids) => {
+            if (!name) return ids
+            const set = new Set(ids)
+            set.add(name)
+            return Array.from(set)
+          })
+        }, [name, setWorkspaceIds])}>
+        Create Workspace
+      </button>
+    </div>
+  )
+}
+
+const SwitchWorkspace = () => {
+  const ids = useAtomValue(workspaceIdsAtom)
+  const setCurrentId = useSetAtom(currentWorkspaceIdAtom)
+  return (
+    <select
+      style={{
+        position: 'absolute',
+        zIndex: 10000,
+      }}
+      onChange={useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+        setCurrentId((id) => {
+          return e.target.value
+        })
+      }, [])}>
+      {ids.map((id) => {
+        return (
+          <option key={id} value={id}>
+            {id}
+          </option>
+        )
+      })}
+    </select>
+  )
+}
 
 function App() {
   const ids = useAtomValue(workspaceIdsAtom)
   const [currentWorkspaceId, setCurrentWorkspaceId] = useAtom(currentWorkspaceIdAtom)
-  if (currentWorkspaceId === null && ids.length > 0) {
-    setCurrentWorkspaceId(ids[0])
-  }
+  useEffect(() => {
+    if (currentWorkspaceId === null && ids.length > 0) {
+      setCurrentWorkspaceId(ids[0])
+    }
+  }, [currentWorkspaceId, ids, setCurrentWorkspaceId])
+
   if (!currentWorkspaceId) {
-    return <div>No Workspace</div>
+    return <CreateWorkspace />
   }
   return (
     <div>
+      <SwitchWorkspace />
       <Suspense fallback={<div>loading</div>}>
-        <Editor />
+        <Editor key={currentWorkspaceId} />
       </Suspense>
     </div>
   )
